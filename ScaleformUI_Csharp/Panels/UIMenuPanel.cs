ed562784949d735ff4ccf06046b45defa2e94d0a
Leonardo Emanuele <manups4e@gmail.com>
2022-02-04 14:46:09 +0100

    UIMenuPanel removed unused parameters

--- ScaleformUI_Csharp/Panels/UIMenuPanel.cs
+++ ScaleformUI_Csharp/Panels/UIMenuPanel.cs
@@ -9,23 +9,14 @@ namespace ScaleformUI
 {
 	public class UIMenuPanel
 	{
-		internal dynamic Background;
-		protected SizeF Resolution = ScreenTools.ResolutionMaintainRatio;
 		public UIMenuPanel()
 		{
 		}
 		public virtual bool Selected { get; set; }
 		public virtual bool Enabled { get; set; }
-		internal virtual void Position(float y)
-		{
-		}
 		public virtual void UpdateParent()
 		{
 		}
-		internal async virtual Task Draw()
-		{
-		}
-
 		public void SetParentItem(UIMenuItem item)
 		{
 			ParentItem = item;
