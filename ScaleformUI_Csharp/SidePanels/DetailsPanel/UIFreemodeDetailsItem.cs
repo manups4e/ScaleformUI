676744432adaf32293d01d00058249559bd34145
Leonardo Emanuele <manups4e@gmail.com>
2022-02-04 14:45:04 +0100

    Added new Details SidePanel

---
+++ ScaleformUI_Csharp/SidePanels/DetailsPanel/UIFreemodeDetailsItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaleformUI
+{
+    public class UIFreemodeDetailsItem
+    {
+        public string TextLeft;
+        public string TextRight;
+        public BadgeIcon Icon;
+        public HudColor IconColor;
+        public int Type;
+        public bool Tick;
+        public UIFreemodeDetailsItem(string textLeft, string textRight, bool separator)
+        {
+            Type = separator ? 3 : 0;
+            TextLeft = textLeft;
+            TextRight = textRight;
+        }
+        public UIFreemodeDetailsItem(string textLeft, string textRight, BadgeIcon icon, HudColor iconColor = HudColor.HUD_COLOUR_FREEMODE, bool tick = false)
+        {
+            Type = 2;
+            TextLeft = textLeft;
+            TextRight = textRight;
+            Icon = icon;
+            IconColor = iconColor;
+            Tick = tick;
+        }
+    }
+}
---
+++ ScaleformUI_Csharp/SidePanels/DetailsPanel/UIMissionDetailsPanel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaleformUI
+{
+    public enum MissionDetailsTitleType
+    {
+        Big,
+        Small,
+        Classic
+    }
+    public class UIMissionDetailsPanel : UIMenuSidePanel
+    {
+        public string TextureDict;
+        public string TextureName;
+        internal MissionDetailsTitleType _titleType;
+        public string Title;
+        public HudColor TitleColor;
+        public List<UIFreemodeDetailsItem> Items = new();
+
+        /// <summary>
+        /// Adds a Mission Details panel as side menu panel
+        /// </summary>
+        /// <param name="side">Left or Right?</param>
+        /// <param name="title">Panel's title</param>
+        /// <param name="inside">if true the title will be within the panel itself else will be the same as the menu.</param>
+        /// <param name="txd">Texture dictionary for the picture</param>
+        /// <param name="txn">Texture name for the picture</param>
+        public UIMissionDetailsPanel(PanelSide side, string title, bool inside, string txd = "", string txn = "")
+        {
+            PanelSide = side;
+            if(inside)
+                _titleType = MissionDetailsTitleType.Classic;
+            else
+                _titleType = MissionDetailsTitleType.Big;
+            Title = title;
+            TitleColor = HudColor.NONE;
+            TextureDict = txd;
+            TextureName = txn;
+        }
+
+        /// <summary>
+        /// Adds a Mission Details panel as side menu panel
+        /// </summary>
+        /// <param name="side">Left or Right?</param>
+        /// <param name="title">Panel's title</param>
+        /// <param name="color">Background color for the panel title</param>
+        /// <param name="txd">Texture dictionary for the picture</param>
+        /// <param name="txn">Texture name for the picture</param>
+        public UIMissionDetailsPanel(PanelSide side, string title, HudColor color, string txd = "", string txn = "")
+        {
+            PanelSide = side;
+            _titleType = MissionDetailsTitleType.Small;
+            Title = title;
+            TitleColor = color;
+            TextureDict = txd;
+            TextureName = txn;
+        }
+
+        /// <summary>
+        /// To change panel's picture
+        /// </summary>
+        /// <param name="txd">Texture dictionary for the picture</param>
+        /// <param name="txn">Texture name for the picture</param>
+        public void UpdatePanelPicture(string txd, string txn)
+        {
+            TextureDict = txd;
+            TextureName = txn;
+            if (ParentItem is not null)
+            {
+                var wid = ParentItem.Parent.MenuItems.IndexOf(ParentItem);
+                ScaleformUI._ui.CallFunction("UPDATE_MISSION_DETAILS_PANEL_IMG", wid, TextureDict, TextureName);
+            }
+
+        }
+
+        /// <summary>
+        /// Adds an item to the description of the panel
+        /// </summary>
+        /// <param name="item">The items to add</param>
+        public void AddItem(UIFreemodeDetailsItem item)
+        {
+            Items.Add(item);
+            if (ParentItem is not null)
+            {
+                var wid = ParentItem.Parent.MenuItems.IndexOf(ParentItem);
+                ScaleformUI._ui.CallFunction("ADD_MISSION_DETAILS_DESC_ITEM", wid, item.Type, item.TextLeft, item.TextRight, (int)item.Icon, (int)item.IconColor, item.Tick);
+            }
+        }
+
+        /// <summary>
+        /// Removes an item from the scription of the panel
+        /// </summary>
+        /// <param name="idx">item's index</param>
+        public void RemoveItem(int idx)
+        {
+            Items.RemoveAt(idx);
+            if (ParentItem is not null)
+            {
+                var wid = ParentItem.Parent.MenuItems.IndexOf(ParentItem);
+                ScaleformUI._ui.CallFunction("REMOVE_MISSION_DETAILS_DESC_ITEM", wid, TextureDict, TextureName);
+            }
+        }
+    }
+}
