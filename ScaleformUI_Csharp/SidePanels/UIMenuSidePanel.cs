573b33f9971165f0e511f145f304a417394931d9
Leonardo Emanuele <manups4e@gmail.com>
2022-02-04 14:46:24 +0100

    added UIMenuSidePanel

---
+++ ScaleformUI_Csharp/SidePanels/UIMenuSidePanel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaleformUI
+{
+	public enum PanelSide
+    {
+		Left,
+		Right
+    }
+    public class UIMenuSidePanel
+    {
+		public virtual bool Selected { get; set; }
+		public virtual bool Enabled { get; set; }
+		public virtual PanelSide PanelSide { get; set; }
+		public virtual void UpdateParent()
+		{
+		}
+		public void SetParentItem(UIMenuItem item)
+		{
+			ParentItem = item;
+		}
+
+		public UIMenuItem ParentItem { get; set; }
+
+	}
+}
