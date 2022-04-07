43facbc6affff1f127764f5cad89b3fb5f23a0b8
Leonardo Emanuele <manups4e@gmail.com>
2022-04-07 11:26:04 +0200

    Lua: Added PauseMenuScaleform metatable and clear everything on resource stop

---
+++ ScaleformUI_Lua/src/scaleforms/PauseMenu/PauseMenuScaleform.lua
@@ -0,0 +1,243 @@
+PauseMenu = {}
+
+local Pause = {}
+Pause = setmetatable({}, Pause)
+
+Pause.__call = function()
+    return true
+end
+Pause.__index = Pause
+
+
+function PauseMenu.New()
+    _data = {
+        _header = nil,
+        _pause = nil,
+        Loaded = false,
+        _visible = false,
+    }
+    return setmetatable(_data, Pause)
+end
+
+function Pause:Visible(visible)
+    if tobool(visible) then
+        self._visible = visible
+    else
+        return self._visible
+    end
+end
+
+function Pause:Load()
+    if(self._header ~= nil and self._pause ~= nil) then return end
+    self._header = Scaleform.Request("pausemenuheader")
+    self._pause = Scaleform.Request("pausemenu")
+    self.Loaded = self._header:IsLoaded() and self._pause:IsLoaded()
+end
+
+function Pause:SetHeaderTitle(title, subtitle, shiftUpHeader)
+    if(subtitle == nil) then subtitle = "" end
+    if(shiftUpHeader == nil) then shiftUpHeader = false end
+    self._header:CallFunction("SET_HEADER_TITLE", false, title, subtitle, shiftUpHeader)
+end
+
+function Pause:SetHeaderDetails(topDetail, midDetail, botDetail)
+    self._header:CallFunction("SET_HEADER_DETAILS", false, topDetail, midDetail, botDetail, false)
+end
+
+function Pause:ShiftCoronaDescription(shiftDesc, hideTabs)
+    self._header:CallFunction("SHIFT_CORONA_DESC", false, shiftDesc, hideTabs)
+end
+
+function Pause:ShowHeadingDetails(show)
+    self._header:CallFunction("SHOW_HEADING_DETAILS", false, show)
+end
+
+function Pause:SetHeaderCharImg(txd, charTexturePath, show)
+    self._header:CallFunction("SET_HEADER_CHAR_IMG", false, txd, charTexturePath, show)
+end
+
+function Pause:SetHeaderSecondaryImg(txd, charTexturePath, show)
+    self._header:CallFunction("SET_HEADER_CREW_IMG", false, txd, charTexturePath, show)
+end
+
+function Pause:HeaderGoRight()
+    self._header:CallFunction("GO_RIGHT", false)
+end
+
+function Pause:HeaderGoLeft()
+    self._header:CallFunction("GO_LEFT", false)
+end
+
+function Pause:AddPauseMenuTab(title, _type)
+    self._header:CallFunction("ADD_HEADER_TAB", false, title)
+    self._pause:CallFunction("ADD_TAB", false, _type)
+end
+
+function Pause:AddLeftItem(tab, _type, title, itemColor, highlightColor)
+    if itemColor == nil then itemColor = Colours.NONE end
+    if highlightColor == nil then highlightColor = Colours.NONE end
+
+    if itemColor ~= Colours.NONE and highlightColor == Colours.NONE then
+        self._pause:CallFunction("ADD_LEFT_ITEM", false, tab, _type, title, itemColor)
+    elseif (itemColor ~= Colours.NONE and highlightColor ~= Colours.NONE) then
+        self._pause:CallFunction("ADD_LEFT_ITEM", false, tab, _type, title, itemColor, highlightColor)
+    else
+        self._pause:CallFunction("ADD_LEFT_ITEM", false, tab, _type, title)
+    end
+
+end
+
+function Pause:AddRightTitle(tab, leftItem, title)
+    self._pause:CallFunction("ADD_RIGHT_TITLE", false, tab, leftItem, title)
+end
+
+function Pause:AddRightListLabel(tab, leftItem, label)
+    AddTextEntry("PauseMenu_"..tab.."_"..leftItem, label)
+    BeginScaleformMovieMethod(self._pause.handle, "ADD_RIGHT_LIST_ITEM")
+    ScaleformMovieMethodAddParamInt(tab)
+    ScaleformMovieMethodAddParamInt(leftItem)
+    ScaleformMovieMethodAddParamInt(0)
+    BeginTextCommandScaleformString("PauseMenu_"..tab.."_"..leftItem)
+    EndTextCommandScaleformString_2()
+    EndScaleformMovieMethod()
+end
+
+function Pause:AddRightStatItemLabel(tab, leftItem, label, rightLabel)
+    self._pause:CallFunction("ADD_RIGHT_LIST_ITEM", false, tab, leftItem, 1, 0, label, rightLabel)
+end
+
+function Pause:AddRightStatItemColorBar(tab, leftItem, label, value, barColor)
+    self._pause:CallFunction("ADD_RIGHT_LIST_ITEM", false, tab, leftItem, 1, 1, label, value, barColor)
+end
+
+function Pause:AddRightSettingsBaseItem(tab, leftItem, label, rightLabel)
+    self._pause:CallFunction("ADD_RIGHT_LIST_ITEM", false, tab, leftItem, 2, 0, label, rightLabel)
+end
+
+function Pause:AddRightSettingsListItem(tab, leftItem, label, items, startIndex)
+    stringList = table.concat(items, ",")
+    self._pause:CallFunction("ADD_RIGHT_LIST_ITEM", false, tab, leftItem, 2, 1, label, stringList, startIndex)
+end
+
+function Pause:AddRightSettingsProgressItem(tab, leftItem, label, max, color, index)
+    self._pause:CallFunction("ADD_RIGHT_LIST_ITEM", false, tab, leftItem, 2, 2, label, max, color, index)
+end
+
+function Pause:AddRightSettingsProgressItemAlt(tab, leftItem, label, max, color, index)
+    self._pause:CallFunction("ADD_RIGHT_LIST_ITEM", false, tab, leftItem, 2, 3, label, max, color, index)
+end
+
+function Pause:AddRightSettingsSliderItem(tab, leftItem, label, max, color, index)
+    self._pause:CallFunction("ADD_RIGHT_LIST_ITEM", false, tab, leftItem, 2, 5, label, max, color, index)
+end
+
+function Pause:AddRightSettingsCheckboxItem(tab, leftItem, label, style, check)
+    self._pause:CallFunction("ADD_RIGHT_LIST_ITEM", false, tab, leftItem, 2, 4, label, style, check)
+end
+
+function Pause:AddKeymapTitle(tab, leftItem, title, rightLabel_1, rightLabel_2)
+    self._pause:CallFunction("ADD_RIGHT_TITLE", false, tab, leftItem, title, rightLabel_1, rightLabel_2)
+end
+
+function Pause:AddKeymapItem(tab, leftItem, label, control1, control2)
+    BeginScaleformMovieMethod(self._pause.handle, "ADD_RIGHT_LIST_ITEM")
+    ScaleformMovieMethodAddParamInt(tab)
+    ScaleformMovieMethodAddParamInt(leftItem)
+    ScaleformMovieMethodAddParamInt(3)
+    PushScaleformMovieFunctionParameterString(label)
+    BeginTextCommandScaleformString("STRING")
+    AddTextComponentScaleform(control1)
+    EndTextCommandScaleformString_2()
+    BeginTextCommandScaleformString("STRING")
+    AddTextComponentScaleform(control2)
+    EndTextCommandScaleformString_2()
+    EndScaleformMovieMethod()
+end
+
+function Pause:UpdateKeymap(tab, leftItem, rightItem, control1, control2)
+    BeginScaleformMovieMethod(self._pause.handle, "UPDATE_KEYMAP_ITEM")
+    ScaleformMovieMethodAddParamInt(tab)
+    ScaleformMovieMethodAddParamInt(leftItem)
+    ScaleformMovieMethodAddParamInt(rightItem)
+    BeginTextCommandScaleformString("string")
+    AddTextComponentScaleform(control1)
+    EndTextCommandScaleformString_2()
+    BeginTextCommandScaleformString("string")
+    AddTextComponentScaleform(control2)
+    EndTextCommandScaleformString_2()
+    EndScaleformMovieMethod()
+end
+
+function Pause:SetRightSettingsItemBool(tab, leftItem, rightItem, value)
+    self._pause:CallFunction("SET_RIGHT_SETTINGS_ITEM_VALUE", false, tab, leftItem, rightItem, value)
+end
+
+function Pause:SetRightSettingsItemIndex(tab, leftItem, rightItem, value)
+    self._pause:CallFunction("SET_RIGHT_SETTINGS_ITEM_VALUE", false, tab, leftItem, rightItem, value)
+end
+
+function Pause:SetRightSettingsItemValue(tab, leftItem, rightItem, value)
+    self._pause:CallFunction("SET_RIGHT_SETTINGS_ITEM_VALUE", false, tab, leftItem, rightItem, value)
+end
+
+function Pause:UpdateItemRightLabel(tab, leftItem, rightItem, label)
+    self._pause:CallFunction("UPDATE_RIGHT_ITEM_RIGHT_LABEL", false, tab, leftItem, rightItem, label)
+end
+
+function Pause:UpdateStatsItemBasic(tab, leftItem, rightItem, label, rightLabel)
+    self._pause:CallFunction("UPDATE_RIGHT_STATS_ITEM", false, tab, leftItem, rightItem, label, rightLabel)
+end
+
+function Pause:UpdateStatsItemBar(tab, leftItem, rightItem, label, value, color)
+    self._pause:CallFunction("UPDATE_RIGHT_STATS_ITEM", false, tab, leftItem, rightItem, label, value, color)
+end
+
+function Pause:UpdateItemColoredBar(tab, leftItem, rightItem, color)
+    if(color == nil or color == Colours.NONE) then
+        self._pause:CallFunction("UPDATE_COLORED_BAR_COLOR", false, tab, leftItem, rightItem, Colours.HUD_COLOUR_WHITE)
+    else
+        self._pause:CallFunction("UPDATE_COLORED_BAR_COLOR", false, tab, leftItem, rightItem, color)
+    end
+end
+
+function Pause:SendInputEvent(direction) -- to be awaited
+    local return_value = self._pause:CallFunction("SET_INPUT_EVENT", true, direction)
+    while not IsScaleformMovieMethodReturnValueReady(return_value) do
+        Citizen.Wait(0)
+    end
+    return GetScaleformMovieFunctionReturnString(return_value)
+end
+
+function Pause:SendScrollEvent(direction) -- to be awaited
+    local return_value = self._pause:CallFunction("SET_SCROLL_EVENT", true, direction, not IsInputDisabled(2))
+    while not IsScaleformMovieMethodReturnValueReady(return_value) do
+        Citizen.Wait(0)
+    end
+    return GetScaleformMovieFunctionReturnString(return_value)
+end
+
+function Pause:SendClickEvent() -- to be awaited
+    local return_value = self._pause:CallFunction("MOUSE_CLICK_EVENT", true)
+    while not IsScaleformMovieMethodReturnValueReady(return_value) do
+        Citizen.Wait(0)
+    end
+    return GetScaleformMovieFunctionReturnString(return_value)
+end
+
+function Pause:Dispose()
+    self._pause:CallFunction("CLEAR_ALL", false)
+    self._header:CallFunction("CLEAR_ALL", false)
+    self._pause:Dispose()
+    self._header:Dispose()
+    _visible = false
+end
+
+function Pause:Draw()
+    if self._visible and not IsPauseMenuActive() then
+        if IsInputDisabled(2) then
+            ShowCursorThisFrame()
+        end
+        self._header:Render2DNormal(0.501, 0.162, 0.6782, 0.145)
+        self._pause:Render2DNormal(0.6617187, 0.7166667, 1.0, 1.0)
+    end
+end
--- ScaleformUI_Lua/src/scaleforms/mainScaleform.lua
+++ ScaleformUI_Lua/src/scaleforms/mainScaleform.lua
@@ -8,6 +8,15 @@ ScaleformUI.Scaleforms.InstructionalButtons = nil
 ScaleformUI.Scaleforms.BigMessageInstance = nil
 ScaleformUI.Scaleforms.Warning = nil
 ScaleformUI.Scaleforms.PlayerListInstance = nil
+ScaleformUI.Scaleforms._pauseMenu = nil
+
+AddEventHandler("onResourceStop", function(resName) 
+    if resName == GetCurrentResourceName() then
+        ScaleformUI.Scaleforms._pauseMenu:Dispose()
+        ScaleformUI.Scaleforms._ui:CallFunction("CLREA_ALL", false)
+        ScaleformUI.Scaleforms._ui:Dispose()
+    end
+end)
 
 Citizen.CreateThread(function()
     ScaleformUI.Scaleforms._ui = Scaleform.Request("scaleformui")
@@ -16,6 +25,9 @@ Citizen.CreateThread(function()
     ScaleformUI.Scaleforms.Warning = WarningInstance.New()
     ScaleformUI.Scaleforms.InstructionalButtons = ButtonsHandler.New()
     ScaleformUI.Notifications = Notifications.New()
+    ScaleformUI.Scaleforms._pauseMenu = PauseMenu.New()
+    ScaleformUI.Scaleforms._pauseMenu:Load()
+    
     while true do
         Wait(0)
         ScaleformUI.Scaleforms.BigMessageInstance:Update()
@@ -24,6 +36,9 @@ Citizen.CreateThread(function()
         ScaleformUI.Scaleforms.Warning:Update()
         if ScaleformUI.Scaleforms._ui == 0 or ScaleformUI.Scaleforms._ui == nil then
             ScaleformUI.Scaleforms._ui = Scaleform.Request("scaleformui")
+        end
+        if(not ScaleformUI.Scaleforms._pauseMenu.Loaded) then
+            ScaleformUI.Scaleforms._pauseMenu:Load()
         end
     end
 end)
