db04e0962ac905ccf73a138370ac7aa9ecf1c554
Leonardo Emanuele <manups4e@gmail.com>
2022-04-08 10:47:07 +0200

    Lua MenuPool: removed old function on menu closed

--- ScaleformUI_Lua/src/base/MenuPool.lua
+++ ScaleformUI_Lua/src/base/MenuPool.lua
@@ -239,7 +239,6 @@ function MenuPool:CloseAllMenus()
     for _, Menu in pairs(self.Menus) do
         if Menu:Visible() then
             Menu:Visible(false)
-            Menu.OnMenuClosed(Menu)
         end
     end
 end
