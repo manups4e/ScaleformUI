7edaa7015c6a07857d39d65594e5b503e0b1a00e
Leonardo Emanuele <manups4e@gmail.com>
2022-04-09 11:18:06 +0200

    Lua: Added AnimationType table

---
+++ ScaleformUI_Lua/src/Elements/AnimationType.lua
@@ -0,0 +1,21 @@
+MenuAnimationType = {
+    LINEAR = 0,
+    QUADRATIC_IN = 1,
+    QUADRATIC_OUT = 2,
+    QUADRATIC_INOUT = 3,
+    CUBIC_IN = 4,
+    CUBIC_OUT = 5,
+    CUBIC_INOUT = 6,
+    QUARTIC_IN = 7,
+    QUARTIC_OUT = 8,
+    QUARTIC_INOUT = 9,
+    SINE_IN = 10,
+    SINE_OUT = 11,
+    SINE_INOUT = 12,
+    BACK_IN = 13,
+    BACK_OUT = 14,
+    BACK_INOUT = 15,
+    CIRCULAR_IN = 16,
+    CIRCULAR_OUT = 17,
+    CIRCULAR_INOUT = 18
+}
