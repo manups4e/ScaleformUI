4078c37d6cea73defa1d44923fdcb9d15e828475
Leonardo Emanuele <manups4e@gmail.com>
2022-04-10 18:14:47 +0200

    Removed unneeded DetailsImageItem.

--- ScaleformUI_Lua/src/sidepanels/UIMenuFreemodeDetailsItem.lua
+++ ScaleformUI_Lua/src/sidepanels/UIMenuFreemodeDetailsItem.lua
@@ -2,16 +2,21 @@ UIMenuFreemodeDetailsItem = setmetatable({}, UIMenuFreemodeDetailsItem)
 UIMenuFreemodeDetailsItem.__index = UIMenuFreemodeDetailsItem
 UIMenuFreemodeDetailsItem.__call = function() return "UIMenuFreemodeDetailsItem", "UIMenuFreemodeDetailsItem" end
 
-function UIMenuFreemodeDetailsItem.New(textLeft, textRight, seperator)
+function UIMenuFreemodeDetailsItem.New(textLeft, textRight, seperator, icon, iconColor, tick)
     if seperator then
         _type = 3
+    elseif icon ~= nil and iconColor ~= nil then
+        _type = 2
     else
         _type = 0
     end
     _UIMenuFreemodeDetailsItem = {
         Type = _type,
         TextLeft = textLeft,
-        TextRight = textRight
+        TextRight = textRight,
+        Icon = icon,
+        IconColor = iconColor,
+        Tick = tick or false
 	}
 	return setmetatable(_UIMenuFreemodeDetailsItem, UIMenuFreemodeDetailsItem)
 end
--- ScaleformUI_Lua/src/sidepanels/UIVehicleColorPickerPanel.lua
+++ ScaleformUI_Lua/src/sidepanels/UIVehicleColorPickerPanel.lua
@@ -2,15 +2,7 @@ UIVehicleColorPickerPanel = setmetatable({}, UIVehicleColorPickerPanel)
 UIVehicleColorPickerPanel.__index = UIVehicleColorPickerPanel
 UIVehicleColorPickerPanel.__call = function() return "UIVehicleColorPickerPanel", "UIVehicleColorPickerPanel" end
 
-function UIVehicleColorPickerPanel.New(side, title, color, inside)
-	if inside == -1 then
-        _titleType = 1
-    elseif inside then
-        _titleType = 2
-    else
-        _titleType = 0
-    end
-
+function UIVehicleColorPickerPanel.New(side, title, color)
     if color ~= -1 then
         _titleColor = color
     else
@@ -39,8 +31,7 @@ end
 
 function UIVehicleColorPickerPanel:UpdatePanelTitle(title)
     self.Title = title
-
-    if self.ParentItem ~= nil then
+    if self.ParentItem ~= nil and self.ParentItem:SetParentMenu() ~= nil and self.ParentItem:SetParentMenu():Visible() then
         local item = IndexOf(self.ParentItem.Base.ParentMenu.Items, self.ParentItem) - 1
         ScaleformUI.Scaleforms._ui:CallFunction("UPDATE_SIDE_PANEL_TITLE", false, item, title)
     end
