d8273303b6d3e98a1e0f8a531362480500e8515d
Wrench <h.kakaeidehkordi@gmail.com>
2022-05-16 23:31:19 +0430

    also change self.Scale to self.Scale.xy/2

--- ScaleformUI_Lua/src/Hud/Marker.lua
+++ ScaleformUI_Lua/src/Hud/Marker.lua
@@ -25,10 +25,6 @@ function Marker.New(type, position, scale, distance, color, placeOnGround, bobUp
 end
 
 function Marker:Draw()
-    -- [Position.Z != _height] means that we make the check only if we change position
-    -- but if we change position and the Z is still the same then we don't need to check again
-    -- We draw it with _height + 0.1 to ensure marker drawing (like horizontal circles)
-
     local ped = PlayerPedId()
     local pedPos = GetEntityCoords(ped, true)
     if(self:IsInRange() and self.PlaceOnGround and self.Position.z ~= self._height +0.1) then
@@ -43,7 +39,7 @@ function Marker:Draw()
     if(self.CheckZ) then
         self.IsInMarker = #(pedPos-self.Position) < #self.Scale/2
     else
-        self.IsInMarker = #(pedPos.xy-self.Position.xy) < #self.Scale/2
+        self.IsInMarker = #(pedPos.xy-self.Position.xy) < #self.Scale.xy/2
     end
 end
  
