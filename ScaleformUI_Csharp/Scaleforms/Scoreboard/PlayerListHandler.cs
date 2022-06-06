d07f258f32fee207f050e358b67429b66376dd12
Leonardo Emanuele <manups4e@gmail.com>
2022-06-06 20:56:05 +0200

    removed weird debug code

--- ScaleformUI_Csharp/Scaleforms/Scoreboard/PlayerListHandler.cs
+++ ScaleformUI_Csharp/Scaleforms/Scoreboard/PlayerListHandler.cs
@@ -80,11 +80,7 @@ namespace ScaleformUI
 
         public void AddRow(PlayerRow row)
         {
-            //if (PlayerRows.Any(x => x.ServerId == row.ServerId)) return;
             PlayerRows.Add(row);
-            //PlayerRows.Sort((row1, row2) => row1.ServerId.CompareTo(row2.ServerId));
-            if (PlayerRows.Any(x => x.RightText.ToLower() == "lobby")) return;
-            PlayerRows.Sort((row1, row2) => Convert.ToInt32(row1.RightText).CompareTo(Convert.ToInt32(row2.RightText)));
         }
 
         public void RemoveRow(PlayerRow row)
@@ -276,5 +272,4 @@ namespace ScaleformUI
         public string TextureString;
         public char FriendType;
     }
-
 }
