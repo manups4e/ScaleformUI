6d6df588d8d76c863d81c8c707df1a95f46cb23a
matthias-codes <matthias00187@gmail.com>
2022-09-09 11:09:12 +0200

    Revert "Update UIMenuGridPanel.lua"
    This reverts commit 5b14bf1b5f0a87e7bd7617fc3f4a549ab45a0567.

--- ScaleformUI_Lua/src/panels/UIMenuGridPanel.lua
+++ ScaleformUI_Lua/src/panels/UIMenuGridPanel.lua
@@ -30,7 +30,7 @@ function UIMenuGridPanel:SetParentItem(Item) -- required
 	end
 end
 
-function UIMenuGridPanel:SetCirclePosition(position)
+function UIMenuGridPanel:CirclePosition(position)
 	if position ~= nil then
 		self.CirclePosition = position
 		if self.ParentItem ~= nil and self.ParentItem:SetParentMenu() ~= nil and self.ParentItem:SetParentMenu():Visible() then
