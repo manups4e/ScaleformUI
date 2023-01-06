472869ff687d61f9367e637a9dbfb778399cbd85
H@mer <13221018+chaixshot@users.noreply.github.com>
2023-01-07 06:41:59 +0700

    Fix pause menu ClonePed on refresh playerlist.

--- ScaleformUI_Lua/src/LobbyMenu/Items/FriendItem.lua
+++ ScaleformUI_Lua/src/LobbyMenu/Items/FriendItem.lua
@@ -61,6 +61,7 @@ function FriendItem:AddPedToPauseMenu(ped)
             end
             local pSubT = self.ParentColumn.Parent()
             Citizen.CreateThread(function()
+                Wait(100)
                 if pSubT == "LobbyMenu" then
                     if self.ParentColumn.Items[self.ParentColumn:CurrentSelection()] == self then
                         local ped = ClonePed(self.ClonePed, false, false, true)
