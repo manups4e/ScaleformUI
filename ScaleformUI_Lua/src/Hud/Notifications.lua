3b0b87b220a5a14539a75f5380016653765a69f1
local9 <apjcookie1@hotmail.com>
2023-04-28 20:39:24 +0100

    chore: add Notifications documentation

--- ScaleformUI_Lua/src/Hud/Notifications.lua
+++ ScaleformUI_Lua/src/Hud/Notifications.lua
@@ -4,6 +4,26 @@ Notifications.__call = function()
     return "Notifications", "Notifications"
 end
 
+---@class Notifications
+---@field private _handle number
+---@field public Type table<string, number>
+---@field public Color table<string, number>
+---@field public NotificationIcon table<string, number>
+---@field public IconChars table<string, string>
+---@field public Hide fun(self:Notifications):nil
+---@field public ShowNotification fun(self:Notifications, msg:string, blink:boolean, showInBrief:boolean):nil
+---@field public ShowNotificationWithColor fun(self:Notifications, msg:string, color:Colours, blink:boolean, showInBrief:boolean):nil
+---@field public ShowHelpNotification fun(self:Notifications, helpText:string, duration:number):nil
+---@field public ShowFloatingHelpNotification fun(self:Notifications, helpText:string, coords:vector3, duration:number):nil
+---@field public ShowAdvancedNotification fun(self:Notifications, title:string, subtitle:string, body:string, character:NotificationCharacters, icon:NotificationIcon, backgroundColor:Colours, flashColoir:table<number, number, number>, blink:boolean, type:NotificationType, sound:string):nil
+---@field public ShowStatNotification fun(self:Notifications, newProgress:number, oldProgress:number, title:string, blink:boolean, showInBrief:boolean):nil
+---@field public ShowVSNotification fun(self:Notifications, ped1:number, ped2:number, colour1:Colours, colour2:Colours):nil
+---@field public DrawText3D fun(self:Notifications, coords:vector3, colour:Colours, text:string, font:Font, size:number):nil
+---@field public DrawText fun(self:Notifications, x:number, y:number, text:string, colour:Colours, font:Font, textAlignment:number, shadow:boolean, outline:boolean, wordWrap:number):nil
+---@field public ShowSubtitle fun(self:Notifications, text:string, duration:number):nil
+
+---Creates a new Notifications object
+---@return Notifications
 function Notifications.New()
     local _notif = {
         _handle = 0,
@@ -140,66 +160,113 @@ function Notifications.New()
     return setmetatable(_notif, Notifications)
 end
 
+---Hide the notification
+---@return nil
 function Notifications:Hide()
     ThefeedRemoveItem(self._handle)
 end
 
-function Notifications:ShowNotification(msg, blink, showBrief)
+---Show a notification
+---@param msg string @The message
+---@param blink boolean @Should the notification blink?
+---@param showInBrief boolean @Should the notification be saved in the brief?
+---@return nil
+function Notifications:ShowNotification(msg, blink, showInBrief)
     AddTextEntry("ScaleformUINotification", msg)
     BeginTextCommandThefeedPost("ScaleformUINotification")
-    EndTextCommandThefeedPostTicker(blink, showBrief)
+    EndTextCommandThefeedPostTicker(blink, showInBrief)
 end
 
-function Notifications:ShowNotificationWithColor(msg, color, blink, showBrief)
+---Show a notification with a color
+---@param msg string @The message
+---@param color Colours @The color
+---@param blink boolean @Should the notification blink?
+---@param showInBrief boolean @Should the notification be saved in the brief?
+---@return nil
+function Notifications:ShowNotificationWithColor(msg, color, blink, showInBrief)
     AddTextEntry("ScaleformUINotification", msg)
     BeginTextCommandThefeedPost("ScaleformUINotification")
     ThefeedSetNextPostBackgroundColor(color)
-    EndTextCommandThefeedPostTicker(blink, showBrief)
+    EndTextCommandThefeedPostTicker(blink, showInBrief)
 end
 
-function Notifications:ShowHelpNotification(helpText, time)
+---Show a help notification
+---@param helpText string @The help text
+---@param duration number @The duration in milliseconds to display help notification, set this to nil if you want to control when it is shown
+---@return nil
+function Notifications:ShowHelpNotification(helpText, duration)
     AddTextEntry("ScaleformUIHelpText", helpText)
-    if (time ~= nil) then
-        if (time > 5000) then time = 5000 end
+    if (duration ~= nil) then
+        if (duration > 5000) then duration = 5000 end
         BeginTextCommandDisplayHelp("ScaleformUIHelpText")
-        EndTextCommandDisplayHelp(0, false, true, time)
+        EndTextCommandDisplayHelp(0, false, true, duration)
     else
         DisplayHelpTextThisFrame("ScaleformUIHelpText", false)
     end
 end
 
-function Notifications:ShowFloatingHelpNotification(msg, coords, time)
-    if (time == nil) then time = -1 end
+---Show a floating help notification
+---@param msg string @The message
+---@param coords vector3 @The coordinates of the notification
+---@param duration number @The display duration in milliseconds (-1 for infinite)
+---@return nil
+function Notifications:ShowFloatingHelpNotification(msg, coords, duration)
+    if (duration == nil) then duration = -1 end
     AddTextEntry("ScaleformUIFloatingHelpText", msg)
     SetFloatingHelpTextWorldPosition(1, coords.x, coords.y, coords.z)
     SetFloatingHelpTextStyle(1, 1, 2, -1, 3, 0)
     BeginTextCommandDisplayHelp("ScaleformUIFloatingHelpText")
-    EndTextCommandDisplayHelp(2, false, false, time)
+    EndTextCommandDisplayHelp(2, false, false, duration)
 end
 
-function Notifications:ShowAdvancedNotification(title, subtitle, text, iconSet, icon, bgColor, flashColor, blink, type,
+---Show an advanced notification
+---@param title string @The title
+---@param subtitle string @The subtitle
+---@param text string @The body of the nofitication
+---@param characterIcon string @The character to be displayed in the notification, use the NotificationCharacters
+---@param notificationIcon NotificationIcon @The icon
+---@param backgroundColour Colours @The background color
+---@param flashColour {R:number, G:number, B:number, A:number} @The flash color (RGBA)
+---@param blink boolean @Should the notification blink?
+---@param notificationType NotificationType @The type of notification
+---@param sound boolean @Should the notification play a sound?
+---@see NotificationCharacters
+---@see NotificationIcon
+---@see Colours
+---@see NotificationType
+---@return nil
+function Notifications:ShowAdvancedNotification(title, subtitle, text, characterIcon, notificationIcon, backgroundColour,
+                                                flashColour, blink, notificationType,
                                                 sound)
-    if (type == nil) then type = self.Type.Default end
-    if (iconSet == nil) then iconSet = self.IconChars.Default end
-    if (icon == nil) then icon = self.NotificationIcon.Default end
-    if (bgColor == nil) then bgColor = -1 end
+    if (notificationType == nil) then notificationType = self.Type.Default end
+    if (characterIcon == nil) then characterIcon = self.IconChars.Default end
+    if (notificationIcon == nil) then notificationIcon = self.NotificationIcon.Default end
+    if (backgroundColour == nil) then backgroundColour = -1 end
     if (blink == nil) then blink = false end
     AddTextEntry("ScaleformUIAdvancedNotification", text)
     BeginTextCommandThefeedPost("ScaleformUIAdvancedNotification")
     AddTextComponentSubstringPlayerName(text)
-    if (bgColor and bgColor ~= -1) then
-        ThefeedSetNextPostBackgroundColor(bgColor)
+    if (backgroundColour and backgroundColour ~= -1) then
+        ThefeedSetNextPostBackgroundColor(backgroundColour)
     end
-    if (flashColor and not blink) then
-        ThefeedSetAnimpostfxColor(flashColor.R, flashColor.G, flashColor.B, flashColor.A)
+    if (flashColour and not blink) then
+        ThefeedSetAnimpostfxColor(flashColour.R, flashColour.G, flashColour.B, flashColour.A)
     end
     if (sound) then PlaySoundFrontend(-1, "DELETE", "HUD_DEATHMATCH_SOUNDSET", true); end
-    return EndTextCommandThefeedPostMessagetext(iconSet, icon, true, type, title, subtitle)
+    return EndTextCommandThefeedPostMessagetext(characterIcon, characterIcon, true, notificationType, title, subtitle)
 end
 
-function Notifications:ShowStatNotification(newProgress, oldProgress, title, blink, showBrief)
+-- TODO: Investigate if newProgress should be a boolean or a number for EndTextCommandThefeedPostStats
+---Show a stat notification
+---@param newProgress number @The new progress
+---@param oldProgress number @The old progress
+---@param title string @The title
+---@param blink boolean @Should the notification blink?
+---@param showInBrief boolean @Should the notification be saved in the brief?
+---@return nil
+function Notifications:ShowStatNotification(newProgress, oldProgress, title, blink, showInBrief)
     if (blink == nil) then blink = false end
-    if (showBrief == nil) then showBrief = false end
+    if (showInBrief == nil) then showInBrief = false end
     AddTextEntry("ScaleformUIStatsNotification", title)
     local handle = RegisterPedheadshot(PlayerPedId())
     while not IsPedheadshotReady(handle) or not IsPedheadshotValid(handle) do Citizen.Wait(0) end
@@ -207,10 +274,17 @@ function Notifications:ShowStatNotification(newProgress, oldProgress, title, bli
     BeginTextCommandThefeedPost("PS_UPDATE")
     AddTextComponentInteger(newProgress)
     EndTextCommandThefeedPostStats("ScaleformUIStatsNotification", 2, newProgress, oldProgress, false, txd, txd)
-    EndTextCommandThefeedPostTicker(blink, showBrief)
+    EndTextCommandThefeedPostTicker(blink, showInBrief)
     UnregisterPedheadshot(handle)
 end
 
+---Show a versuses notification (2 heads)
+---@param ped1 number @The first ped
+---@param ped2 number @The second ped
+---@param color1 Colours @The color of the first ped
+---@param color2 Colours @The color of the second ped
+---@see Colours
+---@return nil
 function Notifications:ShowVSNotification(ped1, ped2, color1, color2)
     local handle_1 = RegisterPedheadshot(ped1)
     while not IsPedheadshotReady(handle_1) or not IsPedheadshotValid(handle_1) do Citizen.Wait(0) end
@@ -228,6 +302,14 @@ function Notifications:ShowVSNotification(ped1, ped2, color1, color2)
     UnregisterPedheadshot(handle_2)
 end
 
+---Put floating text in the world
+---@param coords vector3 @The coordinates of the text
+---@param color {R:number, G:number, B:number, A:number} @The color of the text (RGBA)
+---@param text string @The text
+---@param font Font @The font
+---@param size number @The size
+---@see Font
+---@return nil
 function Notifications:DrawText3D(coords, color, text, font, size)
     local cam = GetGameplayCamCoord()
     local dist = #(coords - cam)
@@ -250,6 +332,18 @@ function Notifications:DrawText3D(coords, color, text, font, size)
     ClearDrawOrigin()
 end
 
+---Draw text on the screen
+---@param x number @The x position of the text (0-1)
+---@param y number @The y position of the text (0-1)
+---@param text string @The text
+---@param color {R:number, G:number, B:number, A:number} @The color of the text (RGBA)
+---@param font Font @The font
+---@param textAlignment number @The text alignment
+---@param shadow boolean @Should the text have a shadow?
+---@param outline boolean @Should the text have an outline?
+---@param wrap number @The wrap
+---@see Font
+---@return nil
 function Notifications:DrawText(x, y, text, color, font, textAlignment, shadow, outline, wrap)
     if (color == nil) then color = { r = 255, g = 255, b = 255, a = 255 } end
     if (font == nil) then font = 4 end
@@ -283,9 +377,13 @@ function Notifications:DrawText(x, y, text, color, font, textAlignment, shadow, 
     EndTextCommandDisplayText(x, y)
 end
 
-function Notifications:ShowSubtitle(msg, time)
-    if (time == nil) then time = 2500 end
+---Add subtitle to the screen
+---@param msg string @The message
+---@param duration number @The duration of how long the subtitle will be displayed (in ms)
+---@return nil
+function Notifications:ShowSubtitle(msg, duration)
+    if (duration == nil) then duration = 2500 end
     AddTextEntry("ScaleformUISubtitle", msg)
     BeginTextCommandPrint("ScaleformUISubtitle")
-    EndTextCommandPrint(time, true)
+    EndTextCommandPrint(duration, true)
 end
