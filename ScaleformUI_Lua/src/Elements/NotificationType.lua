ab5bb7db18343bd59f3d46bc19a320862bdc1b42
127.0.0.1 <kinglo0py@gmail.com>
2023-04-28 20:49:53 +0100

    feat: add multiplayer chat scaleform methods (#103)

---
+++ ScaleformUI_Lua/src/Elements/NotificationType.lua
@@ -0,0 +1,11 @@
+---@enum NotificationType
+NotificationType = {
+  Default = 0,
+  Bubble = 1,
+  Mail = 2,
+  FriendRequest = 3,
+  Default2 = 4,
+  Reply = 7,
+  ReputationPoints = 8,
+  Money = 9
+}
