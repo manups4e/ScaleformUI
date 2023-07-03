5b7a8a625e8386e1fd1764b7253f79eb36f76768
Leonardo Emanuele <manups4e@gmail.com>
2023-07-03 13:05:35 +0200

    ProgressTimerBar automatize timer with time param

--- ScaleformUI_Lua/src/TimerBars/Bars/ProgressTimerBar.lua
+++ ScaleformUI_Lua/src/TimerBars/Bars/ProgressTimerBar.lua
@@ -6,12 +6,17 @@ ProgressTimerBar.__call = function()
     return "TimerBarBase", "ProgressTimerBar"
 end
 
-function ProgressTimerBar.New(label, backgroundColor, foregroundColor)
+function ProgressTimerBar.New(label, backgroundColor, foregroundColor, percentage, time)
     if backgroundColor == nil then backgroundColor = { R = 112, G = 25, B = 25, A = 255 } end
     if foregroundColor == nil then foregroundColor = { R = 224, G = 50, B = 50, A = 255 } end
+    if percentage > 1 then percentage = 1 end
+    if percentage < 0 then percentage = 0 end
+    if time < 0 then time = 0 end
     local _data = {
         _label = label or "",
-        _percentage = 0,
+        _percentage = percentage or 0,
+        _maxTime = time or 0,
+        _time = GetGameTimer(),
         _backgroundColor = backgroundColor or { R = 112, G = 25, B = 25, A = 255 }, --darkred
         _foregroundColor = foregroundColor or { R = 224, G = 50, B = 50, A = 255 }, -- red
         _backgroundRect = UIResRectangle.New(0, 0, 150, 15, backgroundColor.R or 112, backgroundColor.G or 25,
@@ -62,13 +67,23 @@ function ProgressTimerBar:Percentage(val)
         return self._percentage
     else
         self._percentage = val
+        if self._maxTime > 0 then
+            self._time = (self._maxTime / 1.0) * val
+        end
     end
+end
+
+function ProgressTimerBar:Time()
+    return self._time
 end
 
 function ProgressTimerBar:Enabled(bool)
     if bool == nil then
         return self._enabled
     else
+        if bool then
+            self._time = GetGameTimer()
+        end
         self._enabled = bool
     end
 end
@@ -78,9 +93,15 @@ function ProgressTimerBar:Draw(interval)
     local resx, resy = ResolutionMaintainRatio()
     local safex, safey = SafezoneBounds()
 
+    if self._percentage > 0 and self._maxTime > 0 then
+        local t = GetGameTimer() - self._time
+        self._percentage = 1.0 - (t / (self._maxTime / 1.0))
+        if self._percentage < 0 then self._percentage = 0 end
+    end
+    
     UIResText.New(self:Label(), resx - safex - 180, resy - safey - (30 + (4 * interval)), 0.3, 240, 240, 240, 255,
-        self._labelFont, 2)
-        :Draw()
+        self._labelFont, 2):Draw()
+
     Sprite.New("timerbars", "all_black_bg", resx - safex - 298, resy - safey - (40 + (4 * interval)), 300, 37, 0.0, 255,
         255, 255, 180):Draw()
 
